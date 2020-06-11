ea9e72d688dc495a7de22a20e444c2d264a06534
nikperevozchikov1 <nikiperevozchikov@yandex.ru>
2020-06-11 20:44:25 +0400

    Добавьте файлы проекта.

---
+++ UberClone/Constants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace UberClone
+{
+    public static class Constants
+    {
+        public const string GoogleMapsApiKey = "AIzaSyDJq4uD-7L6ecjv98vwmhsfiepfPYD3nqg";
+    }
+}
---
+++ UberClone/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UberClone.Models
+{
+    public static class User
+    {
+        public static string FullName { get; set; } = "Perevozchikov Nikita";
+        public static string ShortName { get; set; } = "Perevozchikov N";
+        public static string UserImage { get; set; } = "https://images.app.goo.gl/hPGezK2TuXK5Af1U6";
+        public static double Rating { get; set; } = 4.79;
+
+    }
+}
